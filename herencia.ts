1cf312cc44a72bfc1516501f0c54ec9161d4c26d
W3401 <W3401@lab.aca>
2020-01-14 17:42:28 -0500

    ejemplo de herencia

---
+++ herencia.ts
@@ -0,0 +1,40 @@
+class Empleado{
+    nombre : string
+    edad : number
+    calcularSueldo(){}
+
+}
+
+class Cocinero extends Empleado{
+    //Sobreescritura
+    calcularSueldo(){
+        return 2500
+    }
+}
+
+class Mesero extends Empleado{
+    calcularSueldo(){
+        return 1200
+    }
+}
+
+class Llamador extends Empleado{
+    calcularSueldo(){
+        return 1000
+    }
+}
+
+let imprimirSueldo = (empleado:Empleado) =>{
+    console.log(`El sueldo es: ${empleado.calcularSueldo()}`);
+};
+
+
+let mainHerencia = () => {
+    let mozo = new Mesero();
+    let cocinero = new Cocinero();
+    let llamador = new Llamador();
+
+    imprimirSueldo(llamador);
+};
+
+mainHerencia();
